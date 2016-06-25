3a8222dfb09adf92825701f89a30d18c9dab3518
amigu <amigu@192.168.1.34>
2016-06-25 16:36:23 +0200

    Afegit cotxe

---
+++ ExemplesObjectes/src/com/amiguet/Cotxe.java
@@ -0,0 +1,27 @@
+package com.amiguet;
+
+public class Cotxe {
+	private int nRodes;
+	private String matricula;
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public void setnRodes(int nRodes) {
+		this.nRodes = nRodes;
+	}
+	public int getnRodes() {
+		return nRodes;
+	}
+
+	
+
+	
+
+	
+	
+	
+}
+
--- ExemplesObjectes/src/com/amiguet/Main.java
+++ ExemplesObjectes/src/com/amiguet/Main.java
@@ -6,6 +6,13 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("calla");
 
+		Cotxe c = new Cotxe();
+		Cotxe b = new Cotxe();
+		Cotxe d = new Cotxe();
+		c.setnRodes(4);
+		b.setnRodes(3);
+		d.setnRodes(5);
+		System.out.println(c.getnRodes() + b.getnRodes() + d.getnRodes());
 	}
 
 }
